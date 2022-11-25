aa2de76e6d7f82bbf1cb8114139a23655f650ee6
gor-dey <gordeychepikov@gmail.com>
2022-11-25 21:39:24 +0600

    first commit

---
+++ swiper.js
@@ -0,0 +1,14 @@
+new Swiper('.swiper', {
+
+    // пагинация
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+
+    },
+
+    slidesPerView: 1.2,
+
+    spaceBetween: 16,
+}) 
+
